9602a63d8fa80eeb331c5ab7507084b138ff31de
Jeremy <jweed262@gmail.com>
2014-03-12 11:44:23 -0400

    working parser

--- pythonParsing/parse_pixy.py
+++ pythonParsing/parse_pixy.py
@@ -10,14 +10,22 @@ def parse(n):
 			s += readable(line) + '\n'
 	return s
 
+def flip(n):
+	s = n[2:] + n[:2]
+	return str(int(s,16))
+
+def flips(n):
+	s = n[2:] + n[:2]
+	return (int(s,16))
 
 def readable(n):
 	s = ''
-	s += 'checksum:' + n[:2] + " "				#all of the specificied registrars of the object block (shifted down one because the sync is turned into a newline)
-	s += 'sig:' + n[2:4] + " "
-	s += '(' + n[4:6] + ',' + n[6:8] + ") "
-	s += "width:" + n[8:10] + " "
-	s += "height" + n[10:12] + " "
+	s += 'checksum:' + flip(n[:4]) + " "				#all of the specificied registrars of the object block (shifted down one because the sync is turned into a newline)
+	s += 'sig:' + flip(n[4:8]) + " "
+	s += '(' + flip(n[8:12]) + ',' + flip(n[12:16]) + ") "
+	s += "width:" + flip(n[16:20]) + " "
+	s += "height:" + flip(n[20:24]) + " "
+	s += "oursum" + str(flips(n[4:8]) + flips(n[8:12]) + flips(n[12:16]) + flips(n[16:20]) + flips(n[20:24])) 
 	return s
 
 filename = raw_input("enter filename: ")		#writing to the file, should be called "readable.txt"
@@ -42,7 +50,3 @@ bigS = parse(f3)								#makes it readable
 f3.close()
 f4.write(bigS)
 f4.close()
-os.remove(preParse)
-
-
-
